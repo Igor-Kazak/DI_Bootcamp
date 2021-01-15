e7c2a26b4d3c79bedd32875ccb0522a5440caa4d
Igor-Kazak <igor@strizhi.ru>
2021-01-15 22:44:10 +0200

    ex1

---
+++ Weel2/Day1/exercise1.js
@@ -0,0 +1,6 @@
+var addressNumber = 77
+var addressStreet = 'Yoqneam'
+var country = 'Israel'
+var global_address
+global_address = 'I live in ' + addressStreet + ' ' + addressNumber + ', in ' + country
+console.log(global_address)
