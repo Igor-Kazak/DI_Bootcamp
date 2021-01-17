a8f32c5d821ad97580fdabf87360fa7fb8334e56
Igor-Kazak <igor@strizhi.ru>
2021-01-17 12:08:52 +0200

    exercises

---
+++ Week2/Day2/exercise4.js
@@ -0,0 +1,15 @@
+var someObject = {
+    username: 'MY NAME',
+    password: 'qwerty12345',
+};
+console.log(someObject);
+
+var database = [someObject];
+console.log(database);
+
+var otherObject = {
+    username: '',
+    timeline: '',
+};
+var newsfeed = [otherObject, otherObject, otherObject];
+console.log(newsfeed);
