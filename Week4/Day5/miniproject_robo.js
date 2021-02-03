48ee960b3dc9d0abe5311b892ddde01db2b3ca51
Igor-Kazak <igor@strizhi.ru>
2021-02-03 19:57:27 +0200

    Mini-project Robo

---
+++ Week4/Day5/miniproject_robo.js
@@ -0,0 +1,122 @@
+const robots = [
+    {
+        id: 1,
+        name: 'Arthur Dent',
+        username: 'Bret',
+        email: 'Sincere@april.biz',
+        image: 'https://robohash.org/1?200x200'
+    },
+    {
+        id: 2,
+        name: 'Ford Prefect',
+        username: 'Antonette',
+        email: 'Shanna@melissa.tv',
+        image: 'https://robohash.org/2?200x200'
+    },
+    {
+        id: 3,
+        name: 'Zaphod Beeblebrox',
+        username: 'Samantha',
+        email: 'Nathan@yesenia.net',
+        image: 'https://robohash.org/3?200x200'
+    },
+    {
+        id: 4,
+        name: 'Marvin',
+        username: 'Karianne',
+        email: 'Julianne.OConner@kory.org',
+        image: 'https://robohash.org/4?200x200'
+    },
+    {
+        id: 5,
+        name: 'Trillian',
+        username: 'Kamren',
+        email: 'Lucio_Hettinger@annie.ca',
+        image: 'https://robohash.org/5?200x200'
+    },
+    {
+        id: 6,
+        name: 'Slartibartfast',
+        username: 'Leopoldo_Corkery',
+        email: 'Karley_Dach@jasper.info',
+        image: 'https://robohash.org/6?200x200'
+    },
+    {
+        id: 7,
+        name: 'Eddie',
+        username: 'Elwyn.Skiles',
+        email: 'Telly.Hoeger@billy.biz',
+        image: 'https://robohash.org/7?200x200'
+    },
+    {
+        id: 8,
+        name: 'Hotblack Desiato',
+        username: 'Maxime_Nienow',
+        email: 'Sherwood@rosamond.me',
+        image: 'https://robohash.org/8?200x200'
+    },
+    {
+        id: 9,
+        name: 'Prostetnic Vogon Jeltz',
+        username: 'Delphine',
+        email: 'Chaim_McDermott@dana.io',
+        image: 'https://robohash.org/9?200x200'
+    },
+    {
+        id: 10,
+        name: 'Zarniwoop',
+        username: 'Moriah.Stanton',
+        email: 'Rey.Padberg@karina.biz',
+        image: 'https://robohash.org/10?200x200'
+    }
+];
+
+createRobots();
+
+function createRobots(){
+for (let i = 0; i < robots.length; i++) {
+    let div = document.getElementsByClassName('row')[0];
+    let col = document.createElement('div');
+    col.classList.add('col');
+    col.id = robots[i].id;
+    div.appendChild(col);
+    let card = document.createElement('div');
+    card.classList.add('card');
+    col.appendChild(card);
+    let img = document.createElement('img');
+    img.classList.add('card-img-top');
+    img.setAttribute("src", robots[i].image);
+    card.appendChild(img);
+    let cardbody = document.createElement('div');
+    cardbody.classList.add('card-body');
+    card.appendChild(cardbody);
+    let h5 = document.createElement('h5');
+    h5.classList.add('card-title');
+    h5.textContent = robots[i].name;
+    cardbody.appendChild(h5);
+    let p = document.createElement('p');
+    p.classList.add('card-text');
+    p.textContent = robots[i].email;
+    cardbody.appendChild(p);
+}
+}
+
+function keyupFunction() {
+    let formcontrol = document.getElementsByClassName('form-control')[0];
+    var col = document.getElementsByClassName('col');
+    if (formcontrol.value != '') {
+        for (let i = 0; i < col.length; i++) {
+            col[i].setAttribute('hidden', '');
+        }
+        for (let i = 0; i < robots.length; i++) {
+            if (robots[i].name.toLowerCase().includes(formcontrol.value)) {
+                document.getElementById(robots[i].id).removeAttribute('hidden');
+            }
+        }
+    }
+    else {
+        for (let i = 0; i < col.length; i++) {
+            col[i].removeAttribute('hidden');
+        }
+    }
+}
