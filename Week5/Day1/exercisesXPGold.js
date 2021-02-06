95aedbe91077cd55eb221347bc522145be89a9f9
Igor-Kazak <igor@strizhi.ru>
2021-02-06 16:48:56 +0200

    Exercises XP Gold

---
+++ Week5/Day1/exercisesXPGold.js
@@ -0,0 +1,17 @@
+const content = `
+    <form action="exercisesXPGold_get.html" method="GET">
+    <div>
+        <label for="from">NAME:</label>
+        <input name="from" id="from" value="">
+      </div>
+    <div>
+      <label for="lastname">LAST NAME:</label>
+      <input name="lastname" value="">
+    </div>
+    <div>
+      <button>Submit</button>
+    </div>
+  </form>
+`;
+
+document.body.innerHTML = content;
