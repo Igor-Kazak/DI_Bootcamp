1a2cab4618782376702f4bcb396e9f6b96920c3d
Igor-Kazak <igor@strizhi.ru>
2021-02-10 09:43:59 +0200

    exercise XP Gold

--- Week5/Day3/exercisesXPGold.js
+++ Week5/Day3/exercisesXPGold.js
@@ -5,39 +5,13 @@ var urls = [
     'https://swapi.dev/api/people/4'
   ];
   
-  let p0 = fetch(urls[0])
-    .then(response => {
-      return response.json();
-    })
-    .catch(err => {
-      console.log('Request Failed', err)
-    });
-
-  let p1 = fetch(urls[1])
-    .then(response => {
-      return response.json();
-    })
-    .catch(err => {
-      console.log('Request Failed', err)
-    });
-
-  let p2 = fetch(urls[2])
-    .then(response => {
-      return response.json();
-    })
-    .catch(err => {
-      console.log('Request Failed', err)
-    });
-
-  let p3 = fetch(urls[3])
-    .then(response => {
-      return response.json();
-    })
-    .catch(err => {
-      console.log('Request Failed', err)
-    });
-
-Promise.all([p0, p1, p2, p3])
-  .then(values => {
-    console.log(values);
-  });
+  Promise.all(urls.map(url =>
+    fetch(url).then(obj => obj.json())
+  ))
+  .then(array => {
+    console.log(array[0])
+    console.log(array[1])
+    console.log(array[2])
+    console.log(array[3])
+  })
+  .catch(err => console.log('Error!', err));
